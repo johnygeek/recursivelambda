51a463479d94697662d44b134bc282eacf0291dc
johnygeek <salil.vaishya@gmail.com>
2017-05-11 14:03:31 +0530

    Added the sample for recursuve lambda

---
+++ src/RecursiveLambdas/RecursiveLambdas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int> fact = null;
+            fact = (n) => n == 1 ? 1 : n * fact(n - 1);
+            Console.WriteLine(fact(5));
+            Console.ReadKey();
+        }
+    }
+}
